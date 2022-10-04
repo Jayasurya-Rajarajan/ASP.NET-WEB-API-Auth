efca61aba6e96b5cf51b8a2dd399ff0e8baf2287
Jayasurya260528 <jayasurya260528@gmail.com>
2022-10-04 12:28:26 +0530

    first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+using BasicAuth.Models;
+
+namespace BasicAuth.Controllers
+{
+    
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [BasicAuthentication]
+        public HttpResponseMessage Get()
+        {
+            string username = Thread.CurrentPrincipal.Identity.Name;
+            using(AuthPracticeEntities entities = new AuthPracticeEntities())
+            {
+                if (username.ToLower() == "male")
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(p => p.Gender.ToLower() == "male").ToList());
+                }
+                else if(username.ToLower() == "female")
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(p => p.Gender.ToLower() == "female").ToList());
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest);
+                }
+
+            }
+        }
+
+        // GET api/values/5
+        
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ EmployeeSecurity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BasicAuth.Models;
+
+namespace BasicAuth
+{
+    public class EmployeeSecurity
+    {
+        public static bool Login(string userName, string password)
+        {
+            using(AuthPracticeEntities entities = new AuthPracticeEntities())
+            {
+                return entities.Users.Any(user => user.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase)
+                && user.Gender == password);
+            }
+        }
+    }
+}
